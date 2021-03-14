a5290aca91babde5ed971f7a8468a232713d56ed
jan <techn@kampherbeek.com>
2021-03-14 19:29:03 +0100

    Completed internationalisation.

---
+++ src/main/kotlin/com/radixpro/enigma/libfe/texts/Rosetta.kt
@@ -0,0 +1,64 @@
+/*
+ * Jan Kampherbeek, (c) 2021.
+ * EnigmaLibFe is open source.
+ * Please check the file copyright.txt in the root of the source for further details.
+ *
+ */
+
+package com.radixpro.enigma.libfe.texts
+
+import java.util.*
+
+/**
+ * i18N manager, takes care of Resource Bundles.<br>
+ * Implemented as a singleton.<br>
+ * setLanguage() needs to be called before using this class. Otherwise Rosetta always uses English.
+ */
+object Rosetta {
+    private var resourceBundle: ResourceBundle? = null
+    private var helpResourceBundle: ResourceBundle? = null
+    private const val RB_LOCATION = "texts"
+    private const val RB_HELP_LOCATION = "help"
+    private const val DUTCH = "du"
+    private const val ENGLISH = "en"
+    private const val PROP_LANG = "lang"
+    private var initialized = false
+    private var language = "en"
+    private var locale: Locale = Locale(ENGLISH, ENGLISH.toUpperCase())
+
+    /**
+     * Sets new language
+     *
+     * @param language use "en" for English or "du" for Dutch (case-sensitive).
+     */
+    fun setLanguage(language: String) {
+        this.language = language
+        if (language == ENGLISH || language == DUTCH) reInitialize()
+    }
+
+    private fun reInitialize() {
+        initi18N()
+        defineResourceBundles()
+        initialized = true
+    }
+
+    private fun initi18N() {
+        locale = if (language == DUTCH) Locale(DUTCH, DUTCH.toUpperCase()) else Locale(ENGLISH, ENGLISH.toUpperCase())
+    }
+
+    private fun defineResourceBundles() {
+        resourceBundle = ResourceBundle.getBundle(RB_LOCATION, locale)
+        helpResourceBundle = ResourceBundle.getBundle(RB_HELP_LOCATION, locale)
+    }
+
+    fun getText(key: String): String {
+        if (!initialized) reInitialize()
+        return resourceBundle!!.getString(key)
+    }
+
+    fun getHelpText(key: String): String {
+        if (!initialized) reInitialize()
+        return helpResourceBundle!!.getString(key)
+    }
+
+}
---
+++ src/test/kotlin/com/radixpro/enigma/libfe/texts/RosettaTest.kt
@@ -0,0 +1,42 @@
+/*
+ * Jan Kampherbeek, (c) 2021.
+ * EnigmaLibFe is open source.
+ * Please check the file copyright.txt in the root of the source for further details.
+ *
+ */
+
+package com.radixpro.enigma.libfe.texts
+
+import io.kotest.matchers.shouldBe
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RosettaTest {
+
+
+    @Test
+    fun `After setting language to dutch, Rosetta should return dutch texts`() {
+        Rosetta.setLanguage("du")
+        Rosetta.getText("test.property") shouldBe "Declinatie"
+    }
+
+    @Test
+    fun `After setting language to english, Rosetta should return english texts`() {
+        Rosetta.setLanguage("en")
+        Rosetta.getText("test.property") shouldBe "Declination"
+    }
+
+
+    @Test
+    fun `Using english, Rosetta should return the correct help text`() {
+        Rosetta.setLanguage("en")
+        Rosetta.getHelpText("myscreen.title") shouldBe "Aspects"
+    }
+
+    @Test
+    fun `Using dutch, Rosetta should return the correct help text`() {
+        Rosetta.setLanguage("du")
+        Rosetta.getHelpText("myscreen.title") shouldBe "Aspecten"
+    }
+}
