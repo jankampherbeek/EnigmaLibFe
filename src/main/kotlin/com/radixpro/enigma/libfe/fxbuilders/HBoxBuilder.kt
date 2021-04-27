0c9205cce98121577552fe086b0208a793d20d1c
jan <tech@kampherbeek.com>
2021-04-27 16:31:27 +0200

    Updated HBoxBuilder.

--- src/main/kotlin/com/radixpro/enigma/libfe/fxbuilders/HBoxBuilder.kt
+++ src/main/kotlin/com/radixpro/enigma/libfe/fxbuilders/HBoxBuilder.kt
@@ -7,37 +7,70 @@
 
 package com.radixpro.enigma.libfe.fxbuilders
 
+import javafx.geometry.Insets
 import javafx.scene.Node
 import javafx.scene.layout.HBox
 
 /**
- * Builder for HBox. No defaults.
+ * Builder for HBox.
  */
 class HBoxBuilder {
 
     private var prefWidth = 0.0
     private var prefHeight = 0.0
+    private var spacing = -1.0
+    private var padding: Insets? = null
     private var children: ArrayList<Node> = ArrayList()
 
+    /**
+     * PrefWidth. Optional.
+     */
     fun setPrefWidth(prefWidth: Double): HBoxBuilder {
         this.prefWidth = prefWidth
         return this
     }
 
+    /**
+     * PrefHeight. Optional.
+     */
     fun setPrefHeight(prefHeight: Double): HBoxBuilder {
         this.prefHeight = prefHeight
         return this
     }
 
-    fun setChildren(children: ArrayList<Node>): HBoxBuilder {
-        this.children = children
+    /**
+     * Spacing. Optional, default 0.0.
+     */
+    fun setSpacing(spacing: Double): HBoxBuilder {
+        this.spacing = spacing
         return this
     }
 
+    /**
+     * Padding. Optional, default Insets(0.0).
+     */
+    fun setPadding(padding: Insets): HBoxBuilder {
+        this.padding = padding
+        return this
+    }
+
+    /**
+     * Child nodes to be shown from left to right.
+     */
+    fun setChildren(vararg children: Node): HBoxBuilder {
+        this.children.addAll(children)
+        return this
+    }
+
+    /**
+     * Build returns instance of HBox.
+     */
     fun build(): HBox {
         val hBox = HBox()
         if (prefWidth > 0.0) hBox.prefWidth = prefWidth
         if (prefHeight > 0.0) hBox.prefHeight = prefHeight
+        if (spacing >= 0.0) hBox.spacing = spacing
+        if (null != padding) hBox.padding = padding
         if (children.isNotEmpty()) hBox.children.addAll(children)
         return hBox
     }
--- src/test/kotlin/com/radixpro/enigma/libfe/fxbuilders/HBoxBuilderTest.kt
+++ src/test/kotlin/com/radixpro/enigma/libfe/fxbuilders/HBoxBuilderTest.kt
@@ -10,7 +10,7 @@ package com.radixpro.enigma.libfe.fxbuilders
 import com.radixpro.enigma.libfe.testsupport.JfxTestRunner
 import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
-import javafx.scene.Node
+import javafx.geometry.Insets
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import org.junit.Test
@@ -22,11 +22,16 @@ class HBoxBuilderTest {
     private val margin = 0.00000001
     private val prefHeight = 150.0
     private val prefWidth = 200.0
-    private val children: ArrayList<Node> = arrayListOf(Label("a"), Button("b"))
+    private val spacing = 3.3
+    private val padding = Insets(1.0, 2.0, 1.0, 2.0)
+    private val child1 = Label("a")
+    private val child2 = Button("b")
     private val hBox = HBoxBuilder()
         .setPrefHeight(prefHeight)
         .setPrefWidth(prefWidth)
-        .setChildren(children).build()
+        .setSpacing(spacing)
+        .setPadding(padding)
+        .setChildren(child1, child2).build()
     private val hBoxDefault = HBoxBuilder().build()
 
     @Test
@@ -40,8 +45,19 @@ class HBoxBuilderTest {
     }
 
     @Test
+    fun `HBoxBuilder should handle spacing correctly`() {
+        hBox.spacing shouldBe (spacing plusOrMinus margin)
+    }
+
+    @Test
+    fun `HBoxBuilder should handle padding correctly`() {
+        hBox.padding shouldBe padding
+    }
+
+    @Test
     fun `HBoxBuilder should handle children correctly`() {
-        hBox.children shouldBe children
+        hBox.children.size shouldBe 2
+        hBox.children[1] shouldBe child2
     }
 
     @Test
@@ -52,6 +68,18 @@ class HBoxBuilderTest {
     @Test
     fun `HBoxBuilder should handle default prefHeight correctly`() {
         hBoxDefault.prefHeight shouldBe -1
+    }
+
+
+    @Test
+    fun `HBoxBuilder should handle default spacing correctly`() {
+        println(hBoxDefault.spacing)
+        hBoxDefault.spacing shouldBe 0.0
+    }
+
+    @Test
+    fun `HBoxBuilder should handle default padding correctly`() {
+        hBoxDefault.padding shouldBe Insets(0.0)
     }
 
     @Test
