e72a26f1d37294164e8c49ad78d4234ef1354c4b
jan <tech@kampherbeek.com>
2021-04-28 11:30:59 +0200

    Updated ListViewBuilder.

--- src/main/kotlin/com/radixpro/enigma/libfe/fxbuilders/ListViewBuilder.kt
+++ src/main/kotlin/com/radixpro/enigma/libfe/fxbuilders/ListViewBuilder.kt
@@ -7,34 +7,85 @@
 
 package com.radixpro.enigma.libfe.fxbuilders
 
-import javafx.scene.control.ListView
+import com.jfoenix.controls.JFXListView
+import com.jfoenix.controls.JFXTooltip
+import javafx.scene.control.SelectionMode
 
 /**
- * Builder for ListView. No defaults.
+ * Builder for ListView. Returns instance of JFXListView.
  */
 class ListViewBuilder {
 
     private var prefHeight = 0.0
     private var prefWidth = 0.0
     private var styleClass: String = ""
+    private var selectionMode = SelectionMode.MULTIPLE
+    private var tooltip = JFXTooltip("")
+    private var showTooltip = false
+    private var verticalGap = 0.0
 
+    /**
+     * PrefWidth. Optional.
+     */
     fun setPrefWidth(prefWidth: Double): ListViewBuilder {
         this.prefWidth = prefWidth
         return this
     }
 
+    /**
+     * PrefHeight. Optional.
+     */
     fun setPrefHeight(prefHeight: Double): ListViewBuilder {
         this.prefHeight = prefHeight
         return this
     }
 
+    /**
+     * SelectionMode. Defaults to SelectionMode.MULTIPLE.
+     */
+    fun setSelectionMode(mode: SelectionMode): ListViewBuilder {
+       this.selectionMode = mode
+       return this
+    }
+
+    /**
+     * Tooltip. Optional.
+     */
+    fun setTooltip(tooltip: JFXTooltip): ListViewBuilder {
+        this.tooltip = tooltip
+        return this
+    }
+
+    /**
+     * ShowTooltop. Defaults to false.
+     */
+    fun setShowTooltip(show: Boolean): ListViewBuilder {
+        this.showTooltip = show
+        return this
+    }
+
+    /**
+     * Vertical gap between cells. Optional.
+     */
+    fun setVerticalGap(vGap: Double): ListViewBuilder {
+        this.verticalGap = vGap
+        return this
+    }
+
+    /**
+     * Styleclass, is used after rocessing other styles. Optional.
+     */
     fun setStyleClass(styleClass: String): ListViewBuilder {
         this.styleClass = styleClass
         return this
     }
 
-    fun build(): ListView<String> {
-        val listView = ListView<String>()
+    fun build(): JFXListView<String> {
+        val listView = JFXListView<String>()
+        listView.isShowTooltip = showTooltip
+        if (tooltip.text != "") listView.tooltip = tooltip
+        listView.selectionModel.selectionMode = selectionMode
+        if (0.0 < verticalGap) listView.verticalGap = verticalGap
         if (0.0 < prefHeight) listView.prefHeight = prefHeight
         if (0.0 < prefWidth) listView.prefWidth = prefWidth
         if (styleClass.isNotBlank()) listView.styleClass.add(styleClass)
--- src/test/kotlin/com/radixpro/enigma/libfe/fxbuilders/ListViewBuilderTest.kt
+++ src/test/kotlin/com/radixpro/enigma/libfe/fxbuilders/ListViewBuilderTest.kt
@@ -7,11 +7,12 @@
 
 package com.radixpro.enigma.libfe.fxbuilders
 
+import com.jfoenix.controls.JFXTooltip
 import com.radixpro.enigma.libfe.testsupport.JfxTestRunner
 import io.kotest.matchers.shouldBe
+import javafx.scene.control.SelectionMode
 import org.junit.Test
 
-import org.junit.Assert.*
 import org.junit.runner.RunWith
 
 @RunWith(JfxTestRunner::class)
@@ -19,11 +20,19 @@ class ListViewBuilderTest {
 
     private val prefWidth = 160.0
     private val prefHeight = 240.0
+    private val verticalGap =  12.0
+    private val tooltip = JFXTooltip("tooltip")
+    private val showTooltip = true
+    private val selectionMode = SelectionMode.SINGLE
     private val styleClass = "listViewStyle"
 
     private val listView = ListViewBuilder()
         .setPrefHeight(prefHeight)
         .setPrefWidth(prefWidth)
+        .setSelectionMode(selectionMode)
+        .setTooltip(tooltip)
+        .setShowTooltip(showTooltip)
+        .setVerticalGap(verticalGap)
         .setStyleClass(styleClass).build()
     private val listViewDefault = ListViewBuilder().build()
 
@@ -38,8 +47,28 @@ class ListViewBuilderTest {
     }
 
     @Test
+    fun `ListViewBuilder should handle vertical gap correctly`() {
+        listView.verticalGap shouldBe verticalGap
+    }
+
+    @Test
+    fun `ListViewBuilder should handle tooltip correctly`() {
+        listView.tooltip shouldBe tooltip
+    }
+
+    @Test
+    fun `ListViewBuilder should handle showTooltip correctly`() {
+        listView.isShowTooltip shouldBe showTooltip
+    }
+
+    @Test
+    fun `ListViewBuilder should handle selectionMode correctly`() {
+        listView.selectionModel.selectionMode shouldBe selectionMode
+    }
+
+    @Test
     fun `ListViewBuilder should handle styleClass correctly`() {
-        listView.styleClass[1] shouldBe styleClass
+        listView.styleClass[2] shouldBe styleClass
     }
 
     @Test
@@ -53,8 +82,29 @@ class ListViewBuilderTest {
     }
 
     @Test
+    fun `ListViewBuilder should handle default vertical gap correctly`() {
+        listViewDefault.verticalGap shouldBe 0.0
+    }
+
+
+    @Test
+    fun `ListViewBuilder should handle default tooltip correctly`() {
+        listViewDefault.tooltip shouldBe null
+    }
+
+    @Test
+    fun `ListViewBuilder should handle default showTooltip correctly`() {
+        listViewDefault.isShowTooltip shouldBe false
+    }
+
+    @Test
+    fun `ListViewBuilder should handle defaultselectionMode correctly`() {
+        listViewDefault.selectionModel.selectionMode shouldBe SelectionMode.MULTIPLE
+    }
+
+    @Test
     fun `ListViewBuilder should handle default styleClass correctly`() {
-        listViewDefault.styleClass.size shouldBe 1
+        listViewDefault.styleClass.size shouldBe 2
     }
 
 
